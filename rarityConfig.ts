368e01ae12fb8a31f4c0bef5444421e70cd82ce4
Aaron <aaron34gtt@gmail.com>
2022-02-09 19:20:38 +1100

    make it better

--- rarityConfig.ts
+++ rarityConfig.ts
@@ -1,10 +1,10 @@
 import { Config } from './src/types'
 
 const config: Config = {
-  // weights: {},
-  // getMissingTraitIdentifier: (trait_type?: string) => undefined,
-  // attributesFieldName: 'attributes',
-  // ignoreTraits: []
+  weights: {},
+  getMissingTraitIdentifier: (trait_type?: string) => undefined,
+  attributesFieldName: 'attributes',
+  ignoreTraits: []
 }
 
 export default config
--- src/scripts/analyse.ts
+++ src/scripts/analyse.ts
@@ -1,8 +1,10 @@
 import { readFileSync, writeFileSync } from 'fs'
+import * as fs from 'fs';
 import yargs from 'yargs'
 import { analyse } from '../analysis'
 import seed from '../db/seed'
 import { Token } from '../types'
+//import fetch from 'node-fetch';
 
 const argv = yargs(process.argv.slice(2)).options({
   json: {
@@ -23,10 +25,36 @@ const argv = yargs(process.argv.slice(2)).options({
 }).argv
 
 if ('json' in argv) {
+  console.log(argv.json)
+  const myArray = argv.json.split(".", 2);
+  const max = parseInt(myArray[1]);
+  argv.json = myArray[0];
   console.log('Analysing data...')
-  const rawData = readFileSync(argv.json, { encoding: 'utf-8' })
-  const tokens: Token[] | { tokens: Token[] } = JSON.parse(rawData)
+  let jsondata = []
+  
+
+    for (let i = 1; i <= max; i++) {
+      //fetch(argv.json+i).then(response => jsondata.push(response.json))
+      console.log(i+" of " + max)
+      let rawjson = JSON.parse(fs.readFileSync(argv.json+i+".json").toString())
+      rawjson = { ...rawjson, id: i }
+      jsondata.push(rawjson)
+      //jsondata.push(JSON.parse(fs.readFileSync(argv.json+i+".json").toString()))
+    }
+
+  // for (let i = 0; i < 3; i++) {
+  //   console.log ("Block statement execution no." + i);
+  // }
+  console.log("test")
+  console.log(myArray[1])
+  //forLoop(1);
+  console.log(jsondata.toString())
+  //const rawData = readFileSync(argv.json+".json", { encoding: 'utf-8' })
+  const tokens: Token[] | { tokens: Token[] } = jsondata
+
+  console.log(Array.isArray(tokens))
   const collection = analyse(tokens)
+  
 
   if (argv.saveJson) {
     writeFileSync(
